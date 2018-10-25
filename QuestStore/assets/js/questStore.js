1324f5ccb6cfaf8a020841a4106aaae4be35a454
Mateusz <kaspazza@gmail.com>
2018-10-25 19:11:55 +0200

    navbar experience bar/profile and align of elements inside

--- QuestStore/assets/js/questStore.js
+++ QuestStore/assets/js/questStore.js
@@ -20,6 +20,20 @@ function dzialam(){
 	}
 }
 
+function move() {
+    var elem = document.getElementById("myBar");
+    var width = 1;
+    var id = setInterval(frame, 10);
+    function frame() {
+        if (width >= 100) {
+            clearInterval(id);
+        } else {
+            width++;
+            elem.style.width = width + '%';
+        }
+    }
+} 
+
 window.onload = function(){
 	for(var i = 0; i < document.getElementsByClassName("group-title").length; i++){
 		document.getElementsByClassName("group-title")[i].addEventListener("click", dzialam);	
