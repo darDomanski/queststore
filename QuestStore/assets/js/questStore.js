08e0d42435e936950759acb3d518307d7d85ad83
Mateusz <kaspazza@gmail.com>
2018-10-25 16:38:19 +0200

    upgraded flexiblitiy and upgraded navbar

--- QuestStore/assets/js/questStore.js
+++ QuestStore/assets/js/questStore.js
@@ -1,7 +1,9 @@
 function dzialam(){
 	if(this.nextElementSibling.classList.contains('hide')){
+		var size = ((this.nextElementSibling.getElementsByClassName("thumbnail").length/3 + 1) * this.nextElementSibling.getElementsByClassName("thumbnail")[0].clientHeight)-100  + "px";
 		this.nextElementSibling.classList.add('show');
 		this.nextElementSibling.classList.remove('hide');
+		this.nextElementSibling.setAttribute("style","height:"+size);
 		for(var i=0; i<this.getElementsByClassName("arrow-up").length; i++){
 			this.getElementsByClassName("arrow-down")[i].style.display = "none";
 			this.getElementsByClassName("arrow-up")[i].style.display = "block";
@@ -10,6 +12,7 @@ function dzialam(){
 	} else {
 		this.nextElementSibling.classList.add('hide');
 		this.nextElementSibling.classList.remove('show');
+		this.nextElementSibling.setAttribute("style","height:0");
 		for(var i=0; i<this.getElementsByClassName("arrow-down").length; i++){
 			this.getElementsByClassName("arrow-down")[i].style.display ="block";
 			this.getElementsByClassName("arrow-up")[i].style.display ="none";
