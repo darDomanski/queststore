cbe13635b22335e6fcb0f9ca74be342388e0de52
Michal Wisniewski <mihuwis@gmail.com>
2018-11-22 13:21:29 +0100

    Add basic method implementations in Student Controller

--- src/main/java/com/codecool/MKM/queststore/Controller/BasicStudentController.java
+++ src/main/java/com/codecool/MKM/queststore/Controller/BasicStudentController.java
@@ -1,6 +1,23 @@
 package com.codecool.MKM.queststore.Controller;
 
+import java.util.List;
+
 public class BasicStudentController implements StudentController{
 
 
+    public List<String> seeVallet(String nickName) {
+        return null;
+    }
+
+    public void buyArtifactSingle() {
+
+    }
+
+    public void buyArtifactGroup() {
+
+    }
+
+    public Integer checkLevelOfExp(String nickName) {
+        return null;
+    }
 }
